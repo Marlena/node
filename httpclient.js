4f80aabbfbda7fc20cef01594e035255ef89ec87
Marlena Compton <marlena.compton@gmail.com>
2015-06-19 14:26:45 -0700

    httpclient

---
+++ httpclient.js
@@ -0,0 +1,15 @@
+(function(){
+  var http = require ('http');
+  var url = process.argv[2];
+
+  http.get(url, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function(data){
+      console.log(data);
+    });
+  }).on('error', function(e){
+    console.log('Got error: ', e.message);
+
+  })
+
+})();
