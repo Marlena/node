b71432cf0c8297a5d16a1c5959478d566a9cc1d5
Marlena Compton <marlena.compton@gmail.com>
2015-06-23 16:34:20 -0700

    passing nodeschool httpcollect

--- httpcollect.js
+++ httpcollect.js
@@ -1,7 +1,23 @@
 (function(){
   'use strict';
 
+  var ConcatStream = require ('concat-stream');
   var http = require ('http');
+  var url = process.argv[2];
+
+  http.get(url, function(response){
+
+    response.setEncoding('utf8');
+
+    response.pipe(ConcatStream(function(data){
+       data = data.toString();
+      console.log(data.length);
+      console.log(data);
+    }))
+
+  });
+
+
 
 
 })();
